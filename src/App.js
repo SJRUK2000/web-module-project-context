54bd2d8729bc04cebd97583e74c0c7a588e4b434
Sean-Ubele <seanjohnreedubeleknuckles@gmail.com>
2022-04-21 12:09:24 -0400

    MVP

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+import ProductContext from './context/ProductContext';
+import CartContext from './context/CartContext';
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -12,21 +14,26 @@ function App() {
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
+		setCart([...cart, item]);
 		// add the given item to the cart
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<CartContext.Provider value={cart} >
+				<Navigation  />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+				{/* Routes */}
+				<ProductContext.Provider value={{products, addItem}}>
+					<Route exact path="/">
+						<Products/>
+					</Route>
+				</ProductContext.Provider>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+				<Route path="/cart">
+					<ShoppingCart  />
+				</Route>
+			</CartContext.Provider>
 		</div>
 	);
 }
